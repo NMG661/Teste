c2bad0e42be2e0071cc03a5d9b197da1d1c8ca22
NMG661 <nicolasffbh@gmail.com>
2025-08-28 19:38:31 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const WebSocket = require("ws");
+const WebSocket = require("wss");
 
 const PORT = process.env.PORT || 10000;
 const wss = new WebSocket.Server({ port: PORT });
+const uuid = v4();
+await playerlist.add(uuid);
+const newPlayer = await playerlist.get(uuid);
+const wss = new WebSocket.Server({ server });
+const playerlist = require("./playerlist.js");
+const server = app.listen(PORT, () => {
+    console.log("Server listening on port: " + PORT);
+});
+
 
 wss.on("connection", (ws) => {
   console.log("Novo jogador conectado!");
@@ -9,6 +18,25 @@ wss.on("connection", (ws) => {
   ws.on("message", (message) => {
     console.log("Recebido:", message.toString());
 
+    // Enviar UUID ao cliente
+    socket.send(JSON.stringify({
+        cmd: "joined_server",
+        content: { msg: "Bem-vindo ao servidor!", uuid }
+    }));
+    
+        // Enviar todos os outros jogadores ao novo cliente
+    socket.send(JSON.stringify({
+        cmd: "spawn_network_players",
+        content: {
+            msg: "Spawning network players!",
+            players: await playerlist.getAll()
+    
+            // Enviar jogador local
+    socket.send(JSON.stringify({
+        cmd: "spawn_local_player",
+        content: { msg: "Spawning local (you) player!", player: newPlayer }
+    }));  
+          
     // envia a mensagem para todos os jogadores conectados
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
